6ee22b1e71b2c6e374cc1c6b8502c4c95bcbc4f0
bautista susvielle <lucier125@gmail.com>
2023-01-29 22:55:14 -0300

    segunda escritura

---
+++ js/informacion.js
@@ -0,0 +1,24 @@
+
+
+
+document.getElementById('idiomas').onclick = function() {
+
+    document.getElementById("infoext").innerHTML = "Idiomas Aprendidos: Ingles - Portuguez - Frances - Español"
+    
+
+}
+
+document.getElementById('paises').onclick = function() {
+
+    document.getElementById("infoext").innerHTML = "Paises Recorridos: EEUU - Brazil - Argentina - Francia"
+    
+
+}
+
+
+document.getElementById('vaciar').onclick = function() {
+
+    document.getElementById("infoext").innerHTML = ""
+    
+
+}
